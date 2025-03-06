8774ccfa98801f9836fb818d0ff76a8c546235ca
Vasyliazhenko Dmytro Vitaliyovych <demon4225@gmail.com>
2025-03-06 14:47:45 +0200

    router_fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import AllRecipesPage from "./pages/AllRecipesPage/AllRecipesPage";
 import RecipePage from "./pages/RecipePage/RecipePage";
 import SelectedRecipesPage from "./pages/SelectedRecipesPage/SelectedRecipesPage";
